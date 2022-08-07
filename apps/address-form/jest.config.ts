89887495d1f60dc1fc5e55379e9f1d2c176f77e8
Denis Khrunov <dkhrunov@tenderhub.ru>
2022-08-07 19:34:32 +0300

    chore: [nx migration] update-to-export-default

--- apps/address-form/jest.config.ts
+++ apps/address-form/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
 	displayName: 'address-form',
 	preset: '../../jest.preset.js',
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
--- apps/test/jest.config.ts
+++ apps/test/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-module.exports = {
+export default {
 	displayName: 'test',
 	preset: '../../jest.preset.js',
 	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
