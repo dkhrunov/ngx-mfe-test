637ed5e835e297b1dfa28f6002123455ce94a36f
Denis Khrunov <dkhrunov@nota.tech>
2025-04-08 01:02:50 +0300

    chore: [nx migration] explicit-standalone-flag

--- apps/loaders/src/app/progress-bar/progress-bar.component.ts
+++ apps/loaders/src/app/progress-bar/progress-bar.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'ngx-mfe-test-progress-bar',
-	template: ` <mat-progress-bar mode="indeterminate"></mat-progress-bar> `,
-	styles: [
-		`
+    selector: 'ngx-mfe-test-progress-bar',
+    template: ` <mat-progress-bar mode="indeterminate"></mat-progress-bar> `,
+    styles: [
+        `
 			:host {
 				display: block;
 			}
 		`,
-	],
+    ],
+    standalone: false
 })
 export class ProgressBarComponent {}
--- apps/test/src/app/mfe-test/mfe-test.component.ts
+++ apps/test/src/app/mfe-test/mfe-test.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'ngx-mfe-test-mfe-test',
-	template: ` <p>MFE Component Works!</p> `,
-	styles: [
-		`
+    selector: 'ngx-mfe-test-mfe-test',
+    template: ` <p>MFE Component Works!</p> `,
+    styles: [
+        `
 			:host {
 				display: block;
 			}
 		`,
-	],
+    ],
+    standalone: false
 })
 export class MfeTestComponent {}
