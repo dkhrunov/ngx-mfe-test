47c222a233ad21c4be976273cbca5b48ba929357
Denis Khrunov <therealpanda98@gmail.com>
2025-04-08 01:00:05 +0300

    Update to v18 (#9)
    * chore: [nx migration] migration-v18
    
    * chore: [nx migration] update-angular-cli-version-18-0-0
    
    * chore: [nx migration] update-19-1-0-rename-no-extra-semi
    
    * chore: [nx migration] 19-2-0-move-graph-cache-directory
    
    * chore: [nx migration] add-typescript-eslint-utils
    
    * chore: [nx migration] update-angular-cli-version-18-1-0
    
    * chore: [nx migration] update-19-6-0
    
    * chore: [nx migration] update-angular-cli-version-18-2-0
    
    * chore: обновление на v18
    
    ---------
    
    Co-authored-by: Denis Khrunov <dkhrunov@nota.tech>

--- apps/address-form/webpack.config.js
+++ apps/address-form/webpack.config.js
@@ -1,4 +1,4 @@
 const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
 
-module.exports = withModuleFederation(config);
+module.exports = withModuleFederation(config, { dts: false });
